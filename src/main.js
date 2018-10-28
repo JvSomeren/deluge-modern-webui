2d0dd1dc2f03bf2c4da22282fb56e46231e14d18
Joost van Someren <joost.v.someren@gmail.com>
2018-10-28 21:43:51 +0100

    Add round filter

--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ import store from './store/index'
 import './assets/style/base.scss'
 
 Vue.config.productionTip = false;
+
+Vue.filter( 'round', ( val, decimals = 2 ) => {
+  if ( !val ) return '';
+  const d = Math.pow( 10, decimals );
+
+  return Math.round( val * d ) / d;
+} );
+
 new Vue( {
   router,
   store,
