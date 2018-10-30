74beeb01efbccddcacc95d7f65cdb8dab8eb5e0c
Joost van Someren <joost.v.someren@gmail.com>
2018-10-30 21:11:09 +0100

    Add round to other filters by default

--- src/main.js
+++ src/main.js
@@ -7,11 +7,15 @@ import './assets/style/base.scss'
 
 Vue.config.productionTip = false;
 
-Vue.filter( 'round', ( val, decimals = 2 ) => {
+let round = ( val, decimals = 2 ) => {
   if ( !val ) return '';
   const d = Math.pow( 10, decimals );
 
   return Math.round( val * d ) / d;
+};
+
+Vue.filter( 'round', ( val, decimals = 2 ) => {
+  return round( val, decimals );
 } );
 
 Vue.filter( 'etaToString', ( val ) => {
@@ -19,34 +23,34 @@ Vue.filter( 'etaToString', ( val ) => {
 
   let timeUnit = 'seconds';
 
-  if ( val / 60 > 60 ) {
+  if ( val / 60 > 1 ) {
     timeUnit = 'minutes';
     val /= 60;
   }
 
-  if ( val / 60 > 60 ) {
+  if ( val / 60 > 1 ) {
     timeUnit = 'hours';
     val /= 60;
   }
 
-  return val + ' ' + timeUnit;
+  return round( val ) + ' ' + timeUnit;
 } );
 
 Vue.filter( 'downloadRateToString', ( val ) => {
   if ( !val ) return '';
   let speedUnit = 'B/s';
 
-  if(val / 1000 > 1) {
+  if ( val / 1000 > 1 ) {
     speedUnit = 'KiB/s';
     val /= 1000;
   }
 
-  if(val / 1000 > 1) {
+  if ( val / 1000 > 1 ) {
     speedUnit = 'MiB/s';
     val /= 1000;
   }
 
-  return val + ' ' + speedUnit;
+  return round( val ) + ' ' + speedUnit;
 } );
 
 new Vue( {
